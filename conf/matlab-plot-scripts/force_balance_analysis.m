b3e138c63c5b3ba1652f1cd99eec0bcdae0f317c
Yeshasvitvs <yeshasvitvs@gmail.com>
2020-09-07 16:08:00 +0200

    Update plotting

--- conf/matlab-plot-scripts/force_balance_analysis.m
+++ conf/matlab-plot-scripts/force_balance_analysis.m
@@ -15,7 +15,8 @@ load('/home/yeshi/software/robotology-superbuild/robotology/human-dynamics-estim
 
 subjectMass = 53.5;
 I_g         = [0 0 9.81]';
-I_f_g       = (subjectMass/2) * I_g;
+I_f2_g       = (subjectMass/2) * I_g;
+I_f_g       = (subjectMass) * I_g;
 
 baseLinkName  = 'Pelvis';
 baseLinkIndex = find(strcmp(linkNames, baseLinkName)); 
@@ -28,6 +29,7 @@ rightFootLinkIndex = find(strcmp(linkNames, rightFootLinkName));
 
 left_foot_f_g  = [];
 right_foot_f_g = [];
+b_f_g = [];
 
 %% Get left foot wrench measurements expressed in link frame
 LeftFootMeasuredWrenchInLinkFrame  = data.task1_wrenchMeasurementsInLinkFrame(1:6,:)';
@@ -36,10 +38,17 @@ RightFootMeasuredWrenchInLinkFrame = data.task1_wrenchMeasurementsInLinkFrame(7:
 LeftFootMeasuredWrenchInLinkFrame_corrected_force = [];
 RightFootMeasuredWrenchInLinkFrame_corrected_force = [];
 
+LeftFootMeasuredWrenchInLinkFrame_in_base = [];
+RightFootMeasuredWrenchInLinkFrame_in_base = [];
+
 for i = 1:size(linkData(baseLinkIndex).data.pose, 2) %% Assuming all the time series length is correct
     
     w_R_b_rpy = linkData(baseLinkIndex).data.pose(4:6, i);
     w_R_b = iDynTree.Rotation.RPY(w_R_b_rpy(1), w_R_b_rpy(2), w_R_b_rpy(3));
+    b_R_w = w_R_b.inverse;
+    
+    % Gravity vector in base
+    b_f_g(i,:) = b_R_w.toMatlab * I_f_g;
     
     w_R_left_foot_rpy = linkData(leftFootLinkIndex).data.pose(4:6, i);
     w_R_left_foot = iDynTree.Rotation.RPY(w_R_left_foot_rpy(1), w_R_left_foot_rpy(2), w_R_left_foot_rpy(3));
@@ -49,65 +58,74 @@ for i = 1:size(linkData(baseLinkIndex).data.pose, 2) %% Assuming all the time se
     
     %% gravity wrench expressed in left foot frame
     left_foot_R_w = w_R_left_foot.inverse;  
-    left_foot_f_g(i, :) = left_foot_R_w.toMatlab * I_f_g;
+    left_foot_f2_g(i, :) = left_foot_R_w.toMatlab * I_f2_g;
     
     %% gravity wrench expressed in right foot frame
     right_foot_R_w = w_R_right_foot.inverse;  
-    right_foot_f_g(i, :) = right_foot_R_w.toMatlab * I_f_g;
+    right_foot_f2_g(i, :) = right_foot_R_w.toMatlab * I_f2_g;
     
     %% Corrected force measurements
     LeftFootMeasuredWrenchInLinkFrame_corrected_force(i,:) = left_foot_R_w.toMatlab * LeftFootMeasuredWrenchInLinkFrame(i, 1:3)';
-    RightootMeasuredWrenchInLinkFrame_corrected_force(i,:) = right_foot_R_w.toMatlab * RightFootMeasuredWrenchInLinkFrame(i, 1:3)';
+    RightFootMeasuredWrenchInLinkFrame_corrected_force(i,:) = right_foot_R_w.toMatlab * RightFootMeasuredWrenchInLinkFrame(i, 1:3)';
+    
+%     LeftFootMeasuredWrenchInLinkFrame_in_base(i,:) = b_R_w.toMatlab * w_R_left_foot.toMatlab * LeftFootMeasuredWrenchInLinkFrame_corrected_force(i, 1:3)';
+%     RightFootMeasuredWrenchInLinkFrame_in_base(i,:) = b_R_w.toMatlab * w_R_right_foot.toMatlab * RightFootMeasuredWrenchInLinkFrame_corrected_force(i, 1:3)';
+    
+    LeftFootMeasuredWrenchInLinkFrame_in_base(i,:) = b_R_w.toMatlab * LeftFootMeasuredWrenchInLinkFrame(i, 1:3)';
+    RightFootMeasuredWrenchInLinkFrame_in_base(i,:) = b_R_w.toMatlab * RightFootMeasuredWrenchInLinkFrame(i, 1:3)';
     
 end
 
-
-
-%% Plot foot wrench measurements in link frame and half of weight expressed in same link frame
-
+%% Plot forces in assumed link and xsens base frames
 fH = figure('units','normalized','outerposition',[0 0 1 1]);
 
-plotIndex = 1;
-for s = 1:2:6
+for s = 1:3
     
-    subplot(3,2,s);
-    plot(LeftFootMeasuredWrenchInLinkFrame_corrected_force(:,plotIndex), 'LineWidth', lineWidth);
+    subplot(3,1,s);
+    plot(LeftFootMeasuredWrenchInLinkFrame_in_base(:,s), 'LineWidth', lineWidth);
     hold on;
-    plot(left_foot_f_g(:,plotIndex), 'LineWidth', lineWidth, 'LineStyle', '--');
+    plot(RightFootMeasuredWrenchInLinkFrame_in_base(:,s), 'LineWidth', lineWidth);
     hold on;
-    %         ylim([-400 800])
-    xlabel('Samples', 'FontSize', fontSize);
-    ylabel(wrenchLegendString(plotIndex), 'Interpreter', 'latex', 'FontSize', fontSize);
-    set (gca, 'FontSize' , fontSize)
-    legend('Left Foot Measured Force in Xsens Left Foot', 'Half of Gravity Force in Xsens Left Foot',...
-           'FontSize', fontSize, 'Location', 'Best');
-       
-    plotIndex = plotIndex+1;
-    
-end
-
-
-plotIndex = 1;
-for s = 2:2:6
-    
-    subplot(3,2,s);
-    plot(RightootMeasuredWrenchInLinkFrame_corrected_force(:,plotIndex), 'LineWidth', lineWidth);
-    hold on;
-    plot(right_foot_f_g(:,plotIndex), 'LineWidth', lineWidth, 'LineStyle', '--');
+    plot(LeftFootMeasuredWrenchInLinkFrame_in_base(:,s) + RightFootMeasuredWrenchInLinkFrame_in_base(:,s), 'LineWidth', lineWidth);
     hold on;
     %         ylim([-400 800])
     xlabel('Samples', 'FontSize', fontSize);
-    ylabel(wrenchLegendString(plotIndex), 'Interpreter', 'latex', 'FontSize', fontSize);
+    ylabel(wrenchLegendString(s), 'Interpreter', 'latex', 'FontSize', fontSize);
     set (gca, 'FontSize' , fontSize)
-    legend('Right Foot Measured Force in Xsens Right Foot', 'Half of Gravity Force in Xsens Right Foot',...
+    legend('Left Foot', 'Right Foot', 'Left + Right',...
            'FontSize', fontSize, 'Location', 'Best');
-    
-    plotIndex = plotIndex+1;
-    
 end
 
 a = axes;
-t = title ("Foot measured force vs half of gravity force expressed in respective link frame");
+t = title ("Foot measured forces expressed in Base Frame");
+t.FontSize = fontSize;
+a.Visible = 'off' ;
+t.Visible = 'on' ;
+axis(a,'fill')
+
+
+%% Plot forces in assumed link and xsens base frames
+fH = figure('units','normalized','outerposition',[0 0 1 1]);
+
+for s = 1:3
+    
+    subplot(3,1,s);
+    plot(LeftFootMeasuredWrenchInLinkFrame_in_base(:,s) + RightFootMeasuredWrenchInLinkFrame_in_base(:,s), 'LineWidth', lineWidth);
+    hold on;
+    plot(b_f_g(:,s), 'LineWidth', lineWidth);
+    hold on;
+    plot(LeftFootMeasuredWrenchInLinkFrame_in_base(:,s) + RightFootMeasuredWrenchInLinkFrame_in_base(:,s) - b_f_g(:,s), 'LineWidth', lineWidth);
+    hold on;
+    %         ylim([-400 800])
+    xlabel('Samples', 'FontSize', fontSize);
+    ylabel(wrenchLegendString(s), 'Interpreter', 'latex', 'FontSize', fontSize);
+    set (gca, 'FontSize' , fontSize)
+    legend('Left + Right ', 'Weight', 'Diff',...
+           'FontSize', fontSize, 'Location', 'Best');
+end
+
+a = axes;
+t = title ("Foot measured forces expressed Vs Weight in Base Frame ");
 t.FontSize = fontSize;
 a.Visible = 'off' ;
 t.Visible = 'on' ;
