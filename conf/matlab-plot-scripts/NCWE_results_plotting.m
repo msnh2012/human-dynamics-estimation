b0efed49347db1347799324de97105b0bad5f9ae
Yeshasvitvs <yeshasvitvs@gmail.com>
2021-03-06 16:39:19 +0100

    Update plotting

--- conf/matlab-plot-scripts/NCWE_results_plotting.m
+++ conf/matlab-plot-scripts/NCWE_results_plotting.m
@@ -2,9 +2,15 @@ close all;
 clear all;
 clc;
 
+N=8;
+C = linspecer(N);
+lineStyles = linspecer(N,'qualitative');
+
+
 %% Plot parameters
-fontSize  = 20;
-lineWidth = 3;
+fontSize  = 30;
+legendFontSize  = 20;
+lineWidth = 3.5;
 
 gravity = 9.81;
 subjectMass = 79.5;
@@ -13,23 +19,36 @@ objectMass = 9.55;
 %% Legend or Title Index
 wrenchLegendString = ["$f_x [N]$", "$f_y [N]$", "$f_z [N]$","$m_x [Nm]$", "$m_y [Nm]$", "$m_z [Nm]$"];
 
+dataset = 'static';
+
 %% Load data
-withoutSOT = load('/home/yeshi/software/robotology-superbuild/src/human-dynamics-estimation/conf/xml/testData/NCWE_MatData/static/withoutSOT.mat');
-withSOT = load('/home/yeshi/software/robotology-superbuild/src/human-dynamics-estimation/conf/xml/testData/NCWE_MatData/static/withSOT.mat');
+withoutSOT = load(strcat('/home/yeshi/software/robotology-superbuild/src/human-dynamics-estimation/conf/xml/testData/NCWE_MatData/',dataset ,'/withoutSOT.mat'));
+withSOT = load(strcat('/home/yeshi/software/robotology-superbuild/src/human-dynamics-estimation/conf/xml/testData/NCWE_MatData/', dataset, '/withSOT.mat'));
 
 withoutSOT_time = withoutSOT.data.time/1e9;
 withSOT_time = withSOT.data.time/1e9;
 
 %% Wrench Measurements Increase Under Load
-withoutSOT_leftFeet_Measurements = withoutSOT.data.task2_wrenchEstimatesInWorldFrame(1:6,:)';
-withoutSOT_rightFeet_Measurements = withoutSOT.data.task2_wrenchEstimatesInWorldFrame(7:12,:)';
+withoutSOT_leftFeet_Measurements = withoutSOT.data.task2_wrenchMeasurementsInWorldFrame(1:6,:)';
+withoutSOT_rightFeet_Measurements = withoutSOT.data.task2_wrenchMeasurementsInWorldFrame(7:12,:)';
 
 withoutSOT_Feet_Measurements = withoutSOT_leftFeet_Measurements + withoutSOT_rightFeet_Measurements;
 
-withSOT_leftFeet_Measurements = withSOT.data.task2_wrenchEstimatesInWorldFrame(1:6,:)';
-withSOT_rightFeet_Measurements = withSOT.data.task2_wrenchEstimatesInWorldFrame(7:12,:)';
+withSOT_leftFeet_Measurements = withSOT.data.task2_wrenchMeasurementsInWorldFrame(1:6,:)';
+withSOT_rightFeet_Measurements = withSOT.data.task2_wrenchMeasurementsInWorldFrame(7:12,:)';
 
 withSOT_Feet_Measurements = withSOT_leftFeet_Measurements + withSOT_rightFeet_Measurements;
+
+withoutSOT_leftHand_Measurements = withoutSOT.data.task1_wrenchMeasurementsInWorldFrame(13:18,:)';
+withoutSOT_rightHand_Measurements = withoutSOT.data.task1_wrenchMeasurementsInWorldFrame(19:24,:)';
+
+withoutSOT_Hands_Measurements = withoutSOT_leftHand_Measurements + withoutSOT_rightHand_Measurements;
+
+withSOT_leftHand_Measurements = withSOT.data.task1_wrenchMeasurementsInWorldFrame(13:18,:)';
+withSOT_rightHand_Measurements = withSOT.data.task1_wrenchMeasurementsInWorldFrame(19:24,:)';
+
+withSOT_Hands_Measurements = withSOT_leftHand_Measurements + withSOT_rightHand_Measurements;
+
 
 %% Measured Object Mass
 withoutSOT_measured_object_mass = [];
@@ -72,10 +91,60 @@ end
 withoutSOT_estimated_object_mass = withoutSOT_estimated_object_mass/gravity;
 withSOT_estimated_object_mass = withSOT_estimated_object_mass/gravity;
 
-
-%% Plots
+%% Plot wrench forces
 fH = figure('units','normalized','outerposition',[0 0 1 1]);
+tl = tiledlayout(2,3);
 
+nexttile;
+plot(withSOT_leftHand_Measurements(:,1) + withSOT_rightHand_Measurements(:,1), 'LineWidth', lineWidth);
+hold on;
+plot(withoutSOT_leftHand_Estimates(:,1) + withoutSOT_rightHand_Estimates(:,1), 'LineWidth', lineWidth, 'LineStyle', '--');
+hold on;
+plot(withSOT_leftHand_Estimates(:,1) + withSOT_rightHand_Estimates(:,1), 'LineWidth', lineWidth, 'LineStyle', '--');
+hold on;
+%         ylim([-400 800])
+xlabel('Samples', 'FontSize', fontSize);
+ylabel(wrenchLegendString(1), 'Interpreter', 'latex', 'FontSize', fontSize);
+set (gca, 'FontSize' , fontSize)
+set (gca, 'ColorOrder' , C)
+legend('Measured', 'Estimation without NCWE', 'Estimation with NCWE', 'FontSize', legendFontSize, 'Location', 'Best');
+legend boxoff     
+
+nexttile;
+plot(withSOT_leftHand_Measurements(:,2) + withSOT_rightHand_Measurements(:,2), 'LineWidth', lineWidth);
+hold on;
+plot(withoutSOT_leftHand_Estimates(:,2) + withoutSOT_rightHand_Estimates(:,2), 'LineWidth', lineWidth, 'LineStyle', '--');
+hold on;
+plot(withSOT_leftHand_Estimates(:,2) + withSOT_rightHand_Estimates(:,2), 'LineWidth', lineWidth, 'LineStyle', '--');
+hold on;
+%         ylim([-400 800])
+xlabel('Samples', 'FontSize', fontSize);
+ylabel(wrenchLegendString(2), 'Interpreter', 'latex', 'FontSize', fontSize);
+set (gca, 'FontSize' , fontSize)
+set (gca, 'ColorOrder' , C)
+legend('Measured', 'Estimation without NCWE', 'Estimation with NCWE', 'FontSize', legendFontSize, 'Location', 'Best');
+legend boxoff     
+
+nexttile;
+plot(withSOT_leftHand_Measurements(:,3) + withSOT_rightHand_Measurements(:,3), 'LineWidth', lineWidth);
+hold on;
+plot(withoutSOT_leftHand_Estimates(:,3) + withoutSOT_rightHand_Estimates(:,3), 'LineWidth', lineWidth, 'LineStyle', '--');
+hold on;
+plot(withSOT_leftHand_Estimates(:,3) + withSOT_rightHand_Estimates(:,3), 'LineWidth', lineWidth, 'LineStyle', '--');
+hold on;
+%         ylim([-400 800])
+xlabel('Samples', 'FontSize', fontSize);
+ylabel(wrenchLegendString(3), 'Interpreter', 'latex', 'FontSize', fontSize);
+set (gca, 'FontSize' , fontSize)
+set (gca, 'ColorOrder' , C)
+legend('Measured', 'Estimation without NCWE', 'Estimation with NCWE', 'FontSize', legendFontSize, 'Location', 'Best');
+legend boxoff     
+
+txt = title(tl, "Sum of forces at the hands", 'FontSize', fontSize, 'fontweight','bold');
+txt.Interpreter= 'none'; 
+
+%% Plots object mass value
+nexttile([1 3])
 
 yline(objectMass, '--', '9.55 Kg', 'LineWidth', lineWidth, 'FontSize', fontSize);
 hold on;
@@ -90,11 +159,13 @@ hold on;
 xlabel('Samples', 'FontSize', fontSize);
 ylabel("Mass [$kg$]", 'Interpreter', 'latex', 'FontSize', fontSize);
 set (gca, 'FontSize' , fontSize)
+set (gca, 'ColorOrder' , C)
 legend('Ground Truth (9.55 Kg)', 'Measurement', 'Estimation without NCWE',...
-       'Estimation with NCWE', 'FontSize', fontSize, 'Location', 'Best');
+       'Estimation with NCWE', 'FontSize', legendFontSize, 'Location', 'Best');
+legend boxoff     
 
-a = axes;
-t = title ("Object Mass Value");
-t.FontSize = fontSize;
-a.Visible = 'off' ;
-t.Visible = 'on' ;
+txt = title("Object Mass Value", 'FontSize', fontSize, 'fontweight','bold');
+txt.Interpreter= 'none'; 
+
+%% Save figure
+save2pdf(strcat(dataset + ".pdf"), fH,300);
