e09e07c04748d48253c7ce207ec6d18149ef8cf7
Yeshasvitvs <yeshasvitvs@gmail.com>
2021-04-05 17:35:31 +0200

    Update wrench offset script

--- conf/xml/wrenchoff_script.sh
+++ conf/xml/wrenchoff_script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "removeWrenchOffset" | yarp rpc /Human-with-sot-1e-4/HumanDynamicsEstimator/rpc:i
-echo "removeWrenchOffset" | yarp rpc /Human-with-sot-1e0/HumanDynamicsEstimator/rpc:i
 echo "removeWrenchOffset" | yarp rpc /Human-with-sot-1e4/HumanDynamicsEstimator/rpc:i
+echo "removeWrenchOffset" | yarp rpc /Human-with-sot-1e0/HumanDynamicsEstimator/rpc:i
+echo "removeWrenchOffset" | yarp rpc /Human-with-sot-1e-4/HumanDynamicsEstimator/rpc:i
 
-echo "removeWrenchOffset" | yarp rpc /Human-without-sot-1e-4/HumanDynamicsEstimator/rpc:i
-echo "removeWrenchOffset" | yarp rpc /Human-without-sot-1e0/HumanDynamicsEstimator/rpc:i
 echo "removeWrenchOffset" | yarp rpc /Human-without-sot-1e4/HumanDynamicsEstimator/rpc:i
+echo "removeWrenchOffset" | yarp rpc /Human-without-sot-1e0/HumanDynamicsEstimator/rpc:i
+echo "removeWrenchOffset" | yarp rpc /Human-without-sot-1e-4/HumanDynamicsEstimator/rpc:i
